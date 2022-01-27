2d23f0f2b0e9d834260aac27e6e98e8dd7a5b9ac
ohyooo <i@ohyooo.com>
2022-01-27 22:23:03 +0800

    remove title

--- app/src/main/kotlin/com/ohyooo/calendar/CalendarActivity.kt
+++ app/src/main/kotlin/com/ohyooo/calendar/CalendarActivity.kt
@@ -1,6 +1,7 @@
 package com.ohyooo.calendar
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.ohyooo.calendar.ui.CalendarMain
@@ -8,7 +9,7 @@ import com.ohyooo.calendar.ui.CalendarMain
 class CalendarActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContent {
             CalendarMain()
         }
