8dcacf5684be72ce6cd955f5b5ab97f21b1453ca
ohyooo <i@ohyooo.com>
2022-03-11 17:15:46 +0800

    7.3.0-alpha05

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -17,7 +17,7 @@ object Libs {
     }
 
     object Plugin {
-        const val AGP = "com.android.tools.build:gradle:7.3.0-alpha04"
+        const val AGP = "com.android.tools.build:gradle:7.3.0-alpha05"
         const val KGP = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
     }
 
