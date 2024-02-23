f1a2e73dfa0ab1514c5d5d8ea40e6c3940a68564
i <i@ohyooo.com>
2024-02-23 19:46:38 +0800

    remove ios

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -12,16 +12,6 @@ kotlin {
     jvm("desktop") {
         jvmToolchain(21)
     }
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "shared"
-            isStatic = true
-        }
-    }
 
     sourceSets {
         val commonMain by getting {
@@ -47,15 +37,6 @@ kotlin {
             dependencies {
                 api(compose.preview)
             }
-        }
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
         }
     }
 }
