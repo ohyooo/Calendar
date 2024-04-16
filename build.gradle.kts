3d1b8e3fc8801660643f2585de77135e84a0ce3b
ba la~ ba la~ <8428270+ohyooo@users.noreply.github.com>
2024-04-16 20:39:03 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,3 +48,6 @@ abstract class GitVersionValueSource : ValueSource<String, ValueSourceParameters
         }
     }
 }
+
+val gitVersion = providers.of(GitVersionValueSource::class.java) {}.get()
+extra["gitVersion"] = gitVersion
